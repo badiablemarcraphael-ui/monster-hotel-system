1112d7f6562f1cec7f6aeab8c659c98c4e6ecac3
badiablemarcraphael-ui <badiablemarcraphael@gmail.com>
2026-05-01 11:05:31 +0800

    C

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ def create_app():
     
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
